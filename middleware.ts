f8ab977cbc48d20ade81dd11a739a958130a22e6
Mu2047 <techtonits@gmail.com>
2026-06-30 18:27:00 -0700

    Allow temporary auth debug endpoint

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isProtectedPage = createRouteMatcher(['/dashboard(.*)'])
-const isProtectedApi = createRouteMatcher(['/api/((?!webhooks).*)'])
+const isProtectedApi = createRouteMatcher(['/api/((?!webhooks|debug-auth).*)'])
 
 export default clerkMiddleware(
   (auth, req) => {
